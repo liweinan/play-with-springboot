7bbda6cea849d408987cfbc9626580279b51b192
Weinan Li <l.weinan@gmail.com>
2018-05-02 16:44:31 +0800

    fix for spring boot 2.x

--- src/main/java/io/weli/BootSpringApp.java
+++ src/main/java/io/weli/BootSpringApp.java
@@ -2,7 +2,7 @@ package io.weli;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class BootSpringApp extends SpringBootServletInitializer {
