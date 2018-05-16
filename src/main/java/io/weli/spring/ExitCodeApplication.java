8dc2d86d21b379fec13b4d3bf3d0b1f4284ecbf8
Weinan Li <l.weinan@gmail.com>
2018-05-16 14:03:45 +0800

    .

--- src/main/java/io/weli/spring/ExitCodeApplication.java
+++ src/main/java/io/weli/spring/ExitCodeApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+
+//@SpringBootApplication
 public class ExitCodeApplication {
     @Bean
     public ExitCodeGenerator exitCodeGenerator() {
